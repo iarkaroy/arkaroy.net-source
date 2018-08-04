6e16c2cfd01d6b9ceefab1312bb7893694e2c060
Arka Roy <i.arkaroy@gmail.com>
2018-08-05 01:29:03 +0530

    Setting up webgl in app

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,51 @@ import React, { Component } from 'react';
 import Prismic from 'prismic-javascript';
 import { cover, contain } from './libs/canvas-background-size';
 import { loadImage } from './libs/load-image';
-import {displacementCanvas} from './libs/displacement-canvas';
+import { displacementCanvas } from './libs/displacement-canvas';
+import * as glUtils from './libs/webgl-utils';
+
+const QUAD = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]);
+
+const vertex = `
+#ifdef GL_ES
+precision highp float;
+#endif
+
+attribute vec2 a_position;
+
+void main() {
+    gl_Position = vec4(a_position, 0, 1);
+}
+`;
+
+const fragment = `
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform sampler2D u_texture1;
+uniform sampler2D u_texture2;
+uniform sampler2D u_disp;
+uniform vec2 u_resolution;
+
+void main() {
+    vec2 div = 1.0 / u_resolution.xy;
+    vec2 uv = gl_FragCoord.xy * div;
+    uv.y = 1.0 - uv.y;
+    vec4 color = texture2D(u_disp, uv);
+    gl_FragColor = color;
+}
+`;
 
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data: null
+            canvases: []
         };
+        this._displacementCanvas = null;
+        this._data = null;
     }
 
     async componentDidMount() {
@@ -19,26 +55,58 @@ class App extends Component {
         Prismic.getApi(apiEndpoint).then(api => {
             return api.query(""); // An empty query will return all the documents
         }).then(response => {
-            this.setState({
-                data: response.results
-            })
+            this._data = response.results;
+            this.prepareCanvases();
         }, err => {
             console.log("Something went wrong: ", err);
         });
-        const disp = await displacementCanvas();
-        document.body.appendChild(disp);
+        this._displacementCanvas = await displacementCanvas();
+        this.initWebGL();
     }
 
+    initWebGL = () => {
+        this.gl = glUtils.getWebGLContext(this.canvas);
+        this.program = glUtils.program(this.gl, vertex, fragment);
+        this.buffer = glUtils.buffer(this.gl);
+    };
+
+    draw = () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const { canvases } = this.state;
+        this.texture1 = glUtils.texture(this.gl, width, height, canvases[0]);
+        this.texture2 = glUtils.texture(this.gl, width, height, canvases[1]);
+        this.dispTexture = glUtils.texture(this.gl, width, height, this._displacementCanvas);
+
+        this.gl.useProgram(this.program);
+        this.texture1.bind(0, this.program.u_texture1);
+        this.texture2.bind(1, this.program.u_texture2);
+        this.dispTexture.bind(2, this.program.u_disp);
+        this.buffer.data(QUAD, this.program.a_position, 2);
+        this.gl.uniform2fv(this.program.u_resolution, new Float32Array([width, height]));
+        glUtils.reset(this.gl, width, height, true);
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, QUAD.length / 2);
+    };
+
+    prepareCanvases = async () => {
+        const projects = this.getProjects();
+        var canvases = [];
+        for (let i = 0; i < projects.length; ++i) {
+            let canvas = await this.getCanvas(projects[i]);
+            canvases.push(canvas);
+        }
+        this.setState({ canvases }, this.draw);
+    };
+
     getProjects = () => {
-        const { data } = this.state;
-        if (!data) {
+        if (!this._data) {
             return [];
         }
-        var projects = data.filter(d => {
+        var projects = this._data.filter(d => {
             return d.type === 'project';
         });
         return projects;
-    }
+    };
 
     getCanvas = async (project) => {
         const img = await loadImage(project.data.image.url);
@@ -61,22 +129,15 @@ class App extends Component {
             data[i + 2] = brightness;
         }
 
-        // overwrite original image
         ctx.putImageData(imageData, x, y);
         */
-        document.body.appendChild(ctx.canvas);
+
         return ctx.canvas;
     };
 
     render() {
-        const projects = this.getProjects();
-        const canvases = [];
-        for (let i = 0; i < projects.length; ++i) {
-            this.getCanvas(projects[i]);
-        }
         return (
-            <div>
-            </div>
+            <canvas ref={o => { this.canvas = o }} width={window.innerWidth} height={window.innerHeight} />
         );
     }
 
