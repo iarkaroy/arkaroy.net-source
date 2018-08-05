fbefaa3e44e0867aa48c82c97cdeff5e6e10f56e
Arka Roy <i.arkaroy@gmail.com>
2018-08-05 23:57:00 +0530

    Updated displacements with variations

--- src/js/libs/displacement-canvas.js
+++ src/js/libs/displacement-canvas.js
@@ -1,7 +1,45 @@
-import dispImage from '../../images/disp.jpg';
 import { loadImage } from './load-image';
 
-export const displacementCanvas = async () => {
+export const displacementCanvas = async (name = null) => {
+    var dispImage = require('../../images/displacement/01.jpg');
+    switch (name) {
+        case '01':
+        case 1:
+            dispImage = require('../../images/displacement/01.jpg');
+            break;
+        case '02':
+        case 2:
+            dispImage = require('../../images/displacement/02.jpg');
+            break;
+        case '03':
+        case 3:
+            dispImage = require('../../images/displacement/03.jpg');
+            break;
+        case '04':
+        case 4:
+            dispImage = require('../../images/displacement/04.jpg');
+            break;
+        case '05':
+        case 5:
+            dispImage = require('../../images/displacement/05.jpg');
+            break;
+        case '06':
+        case 6:
+            dispImage = require('../../images/displacement/06.png');
+            break;
+        case '07':
+        case 7:
+            dispImage = require('../../images/displacement/07.jpg');
+            break;
+        case '08':
+        case 8:
+            dispImage = require('../../images/displacement/08.jpg');
+            break;
+        case '09':
+        case 9:
+            dispImage = require('../../images/displacement/09.png');
+            break;
+    }
     const img = await loadImage(dispImage);
     const ctx = document.createElement('canvas').getContext('2d');
     ctx.canvas.width = window.innerWidth;
