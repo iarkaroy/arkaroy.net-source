022d3604fa72053ce6a8025d0c13a6675d042142
Arka Roy <i.arkaroy@gmail.com>
2018-08-22 00:58:00 +0530

    Added lib for shape overlay

---
+++ src/js/libs/shape-overlays.js
@@ -0,0 +1,99 @@
+
+const ease = {
+    cubicInOut: t => t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+};
+
+class ShapeOverlays {
+
+    constructor(elm) {
+        this.elm = elm;
+        this.path = elm.querySelectorAll('path');
+        this.numPoints = Math.ceil(window.innerWidth / 100 * 1.2);
+        this.duration = 800;
+        this.delayPointsArray = [];
+        this.delayPointsMax = 300;
+        this.delayPerPath = 100;
+        this.timeStart = Date.now();
+        this.isOpened = false;
+        this.isAnimating = false;
+    }
+    toggle() {
+        this.isAnimating = true;
+        const range = 4 * Math.random() + 6;
+        for (var i = 0; i < this.numPoints; i++) {
+            const radian = i / (this.numPoints - 1) * Math.PI;
+            this.delayPointsArray[i] = (Math.sin(-radian) + Math.sin(-radian * range) + 2) / 4 * this.delayPointsMax;
+        }
+        if (this.isOpened === false) {
+            this.open();
+        } else {
+            this.close();
+        }
+    }
+    open() {
+        this.isOpened = true;
+        this.elm.classList.add('is-opened');
+        if (this.delayPointsArray.length < this.numPoints) {
+            const range = 4 * Math.random() + 6;
+            for (var i = 0; i < this.numPoints; i++) {
+                const radian = i / (this.numPoints - 1) * Math.PI;
+                this.delayPointsArray[i] = (Math.sin(-radian) + Math.sin(-radian * range) + 2) / 4 * this.delayPointsMax;
+            }
+        }
+        this.timeStart = Date.now();
+        this.renderLoop();
+    }
+    close() {
+        this.isOpened = false;
+        this.elm.classList.remove('is-opened');
+        if (this.delayPointsArray.length < this.numPoints) {
+            const range = 4 * Math.random() + 6;
+            for (var i = 0; i < this.numPoints; i++) {
+                const radian = i / (this.numPoints - 1) * Math.PI;
+                this.delayPointsArray[i] = (Math.sin(-radian) + Math.sin(-radian * range) + 2) / 4 * this.delayPointsMax;
+            }
+        }
+        this.timeStart = Date.now();
+        this.renderLoop();
+    }
+    updatePath(time) {
+        const points = [];
+        for (var i = 0; i < this.numPoints + 1; i++) {
+            points[i] = ease.cubicInOut(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1)) * 100
+        }
+
+        let str = '';
+        str += (this.isOpened) ? `M 0 0 V ${points[0]} ` : `M 0 ${points[0]} `;
+        for (var i = 0; i < this.numPoints - 1; i++) {
+            const p = (i + 1) / (this.numPoints - 1) * 100;
+            const cp = p - (1 / (this.numPoints - 1) * 100) / 2;
+            str += `C ${cp} ${points[i]} ${cp} ${points[i + 1]} ${p} ${points[i + 1]} `;
+        }
+        str += (this.isOpened) ? `V 0 H 0` : `V 100 H 0`;
+        return str;
+    }
+    render() {
+        if (this.isOpened) {
+            for (var i = 0; i < this.path.length; i++) {
+                this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * i)));
+            }
+        } else {
+            for (var i = 0; i < this.path.length; i++) {
+                this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
+            }
+        }
+    }
+    renderLoop() {
+        this.render();
+        if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPointsMax) {
+            requestAnimationFrame(() => {
+                this.renderLoop();
+            });
+        }
+        else {
+            this.isAnimating = false;
+        }
+    }
+}
+
+export default ShapeOverlays;
