1570491b35b873787955b29df57c87caea620c83
Arka Roy <i.arkaroy@gmail.com>
2018-08-25 20:30:18 +0530

    Updated generate-json to include parsed html

--- generate-json.js
+++ generate-json.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const grayMatter = require('gray-matter');
+const remark = require('remark');
+const html = require('remark-html');
 
 const readProjects = () => {
     return new Promise((resolve, reject) => {
@@ -17,15 +19,35 @@ const readProjects = () => {
     });
 };
 
+const htmlify = str => {
+    return new Promise((resolve, reject) => {
+        remark().use(html).process(str, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(String(data));
+        });
+    });
+}
+
+const parseProjectContents = async (projects) => {
+    for (let i = 0; i < projects.length; ++i) {
+        projects[i].html = await htmlify(projects[i].content);
+    }
+    return projects;
+};
+
 const sortProjects = projects => {
     projects.sort((a, b) => {
         return new Date(b.data.date) - new Date(a.data.date);
     });
 };
 
-readProjects().then(projects => {
-    sortProjects(projects);
-    const json = { projects };
-    const file = path.resolve(__dirname, 'data/data.json');
-    fs.writeFileSync(file, JSON.stringify(json), 'utf8');
-});
+readProjects()
+    .then(parseProjectContents)
+    .then(projects => {
+        sortProjects(projects);
+        const json = { projects };
+        const file = path.resolve(__dirname, 'data/data.json');
+        fs.writeFileSync(file, JSON.stringify(json), 'utf8');
+    });
