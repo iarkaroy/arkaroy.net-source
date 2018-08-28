01db133fd508cda3b3685d0065a2d4513ad611a8
Arka Roy <i.arkaroy@gmail.com>
2018-08-29 00:03:18 +0530

    Updated header to change logo color on scroll

--- src/js/components/shared/Header.js
+++ src/js/components/shared/Header.js
@@ -11,20 +11,20 @@ class Header extends Component {
         EventSystem.subscribe('overlay:open', this.openOverlay);
         EventSystem.subscribe('overlay:close', this.closeOverlay);
         EventSystem.subscribe('overlay:block', this.blockOverlay);
-        document.body.addEventListener('scroll', this.handleScroll);
+        window.addEventListener('scroll', this.handleScroll);
     }
 
     componentWillUnmount() {
-        document.body.removeEventListener('scroll', this.handleScroll);
+        window.removeEventListener('scroll', this.handleScroll);
     }
 
     handleScroll = event => {
-        const { scrollTop } = document.body;
+        const { scrollY } = window;
         const rect = this.logoBack.getBoundingClientRect();
         const { top, height } = rect;
-        const winHeight = window.innerHeight;
+        const winHeight = window.document.documentElement.clientHeight;
         const edge = winHeight - (top + height);
-        const diff = scrollTop - edge;
+        const diff = scrollY - edge;
         if (diff >= 0) {
             const newHeight = height - diff;
             this.logoFront.style.height = newHeight >= 0 ? `${newHeight}px` : 0;
