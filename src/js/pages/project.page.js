71148f0ff9cf2376c6efcaf63eedab30931b262c
Arka Roy <i.arkaroy@gmail.com>
2018-09-02 23:21:42 +0530

    Added project meta and link to next project

--- src/js/pages/project.page.js
+++ src/js/pages/project.page.js
@@ -4,11 +4,20 @@ import { isPrerender } from '../libs/isPrerender';
 import * as store from '../store';
 import { Helmet } from 'react-helmet';
 import styles from '../../scss/index.scss';
+import { Link } from '../router';
+
+const classNames = {
+    metaWrapper: 'project-meta--wrapper',
+    metaInfo: 'project-meta--info',
+    nextWrapper: 'next-project--wrapper',
+    nextInfoWrapper: 'next-project--info-wrapper',
+    nextInfo: 'next-project--info'
+};
 
 class ProjectPage extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor(props, context) {
+        super(props, context);
         this.state = {
             project: null,
             next: null,
@@ -20,17 +29,8 @@ class ProjectPage extends Component {
 
     componentDidMount() {
         EventSystem.publish('overlay:block');
-        if (!isPrerender()) {
-            setTimeout(() => {
-                EventSystem.publish('overlay:close');
-            }, 200);
-        }
         const slug = this.props.params.id
-        this.setState({
-            project: store.project(slug),
-            next: store.next(slug),
-            prev: store.prev(slug)
-        });
+        this.loadProject(slug);
         window.addEventListener('resize', this.handleResize);
         this.handleResize();
     }
@@ -43,6 +43,28 @@ class ProjectPage extends Component {
         EventSystem.publish('overlay:open');
         setTimeout(callback, 850);
     }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const prevSlug = prevProps.params.id;
+        const slug = this.props.params.id
+        if(slug !== prevSlug) {
+            this.loadProject(slug);
+        }
+    }
+
+    loadProject = slug => {
+        this.setState({
+            project: store.project(slug),
+            next: store.next(slug),
+            prev: store.prev(slug)
+        });
+        window.scrollTo(0, 0);
+        if (!isPrerender()) {
+            setTimeout(() => {
+                EventSystem.publish('overlay:close');
+            }, 200);
+        }
+    };
 
     handleResize = event => {
         const { clientWidth, clientHeight } = window.document.documentElement;
@@ -61,9 +83,11 @@ class ProjectPage extends Component {
         const thumb = `/images/${data.thumb}`;
         return (
             <main className={styles.projectSingle}>
+
                 <Helmet>
                     <title>{data.title} - Arka Roy - Web Developer</title>
                 </Helmet>
+
                 <div className={styles.background} style={{ backgroundImage: `url('${thumb}')`, height: height }}></div>
                 <div className={styles.header} style={{ height: height }}>
                     <div className={styles.content}>
@@ -73,7 +97,30 @@ class ProjectPage extends Component {
                 </div>
                 <div className={styles.body}>
                     {data.summary && <div className={styles.summary}>{data.summary}</div>}
+                    <div className={styles[classNames.metaWrapper]}>
+                        <div className={styles[classNames.metaInfo]}>
+                            <h5>Services</h5>
+                            <h4>{data.categories.join(', ')}</h4>
+                        </div>
+                        <div className={styles[classNames.metaInfo]}>
+                            <h5>Year</h5>
+                            <h4>{(new Date(data.date)).getFullYear()}</h4>
+                        </div>
+                    </div>
                     <div className={styles.content} dangerouslySetInnerHTML={{ __html: project.html }}></div>
+                    {
+                        next &&
+                        <div className={styles[classNames.nextWrapper]}>
+                            <div className={styles[classNames.nextInfoWrapper]} style={{ backgroundImage: `url('/images/${next.data.thumb}')` }}>
+                                <Link to={`/projects/${next.data.slug}`} className={styles[classNames.nextInfo]}>
+                                    <strong>Next</strong>
+                                    <h3>{next.data.title}</h3>
+                                    <h4>{next.data.categories.join(', ')}</h4>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
+                                </Link>
+                            </div>
+                        </div>
+                    }
                 </div>
 
             </main>
