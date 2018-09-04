5f725ce49c4e70b19d3e19e9f1bbcd1bbe25a6d7
Arka Roy <i.arkaroy@gmail.com>
2018-09-04 23:48:01 +0530

    Updated json generator to use sync process and included articles

--- generate-json.js
+++ generate-json.js
@@ -3,51 +3,48 @@ const fs = require('fs');
 const grayMatter = require('gray-matter');
 const remark = require('remark');
 const html = require('remark-html');
+const hljs = require('remark-highlight.js');
 
-const readProjects = () => {
-    return new Promise((resolve, reject) => {
-        const dirPath = path.resolve(__dirname, 'data/projects');
-        var projects = [];
-        fs.readdir(dirPath, (err, files) => {
-            files.forEach(file => {
-                const fileContent = fs.readFileSync(path.resolve(dirPath, file), 'utf8');
-                const matter = grayMatter(fileContent);
-                projects.push(matter);
-            });
-            resolve(projects);
-        });
-    });
+const projectsDir = path.resolve(__dirname, 'data/projects');
+const articlesDir = path.resolve(__dirname, 'data/articles');
+
+const getFilesInDir = dir => {
+    return fs.readdirSync(dir);
 };
 
-const htmlify = str => {
-    return new Promise((resolve, reject) => {
-        remark().use(html).process(str, (err, data) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(String(data));
-        });
-    });
-}
-
-const parseProjectContents = async (projects) => {
-    for (let i = 0; i < projects.length; ++i) {
-        projects[i].html = await htmlify(projects[i].content);
-    }
-    return projects;
+const getMatter = filepath => {
+    const fileContent = fs.readFileSync(filepath, 'utf8');
+    const matter = grayMatter(fileContent);
+    return matter;
 };
 
-const sortProjects = projects => {
-    projects.sort((a, b) => {
-        return new Date(b.data.date) - new Date(a.data.date);
-    });
+const parseMarkdown = md => {
+    const parsed = remark().use([html, hljs]).processSync(md);
+    return String(parsed);
 };
 
-readProjects()
-    .then(parseProjectContents)
-    .then(projects => {
-        sortProjects(projects);
-        const json = { projects };
-        const file = path.resolve(__dirname, 'data/data.json');
-        fs.writeFileSync(file, JSON.stringify(json), 'utf8');
-    });
+const projectFiles = getFilesInDir(projectsDir);
+var projects = [];
+projectFiles.forEach(file => {
+    const matter = getMatter(path.resolve(projectsDir, file));
+    matter.html = parseMarkdown(matter.content);
+    projects.push(matter);
+});
+projects.sort((a, b) => {
+    return new Date(b.data.date) - new Date(a.data.date);
+});
+
+const articleFiles = getFilesInDir(articlesDir);
+var articles = [];
+articleFiles.forEach(file => {
+    const matter = getMatter(path.resolve(articlesDir, file));
+    matter.html = parseMarkdown(matter.content);
+    articles.push(matter);
+});
+articles.sort((a, b) => {
+    return new Date(b.data.date) - new Date(a.data.date);
+});
+
+const json = { projects, articles };
+const file = path.resolve(__dirname, 'data/data.json');
+fs.writeFileSync(file, JSON.stringify(json), 'utf8');
