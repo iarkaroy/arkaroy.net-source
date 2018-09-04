339c574082ea6a0e87b5fa23379080daee5e633b
Arka Roy <i.arkaroy@gmail.com>
2018-09-04 23:53:10 +0530

    Added article pages in routes

--- src/js/router/routes.js
+++ src/js/router/routes.js
@@ -1,6 +1,8 @@
 
 import HomePage from '../pages/home.page';
 import ProjectPage from '../pages/project.page';
+import ArticlesPage from '../pages/ArticlesPage';
+import ArticlePage from '../pages/ArticlePage';
 
 const routes = [
     {
@@ -11,6 +13,15 @@ const routes = [
     {
         path: '/projects/:id',
         component: ProjectPage
+    },
+    {
+        path: '/articles',
+        exact: true,
+        component: ArticlesPage
+    },
+    {
+        path: '/articles/:slug',
+        component: ArticlePage
     }
 ];
 export default routes;
