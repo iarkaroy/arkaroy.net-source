3865dfa000931485a26afc5f6f5e8b13cfd369e2
Arka Roy <i.arkaroy@gmail.com>
2018-10-14 20:30:49 +0530

    Updated imgToCanvas to handle image also

--- src/js/libs/imgToCanvas.js
+++ src/js/libs/imgToCanvas.js
@@ -6,33 +6,37 @@ export async function imgToCanvas(src, owidth, oheight, isGrey = false) {
     return new Promise((resolve, reject) => {
 
         loadImage(src).then(img => {
-
-            const ctx = document.createElement('canvas').getContext('2d');
-            ctx.canvas.width = owidth;
-            ctx.canvas.height = oheight;
-            const { width, height, offsetX, offsetY } = cover(ctx.canvas.width, ctx.canvas.height, img.width, img.height);
-            ctx.drawImage(img, offsetX, offsetY, width, height);
-
-            if (isGrey) {
-                const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
-                const { data } = imageData;
-                for (var i = 0; i < data.length; i += 4) {
-                    // var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-                    var brightness = 0.2126 * data[i] + 0.7152 * data[i + 1] + 0.0722 * data[i + 2];
-                    // red
-                    data[i] = brightness;
-                    // green
-                    data[i + 1] = brightness;
-                    // blue
-                    data[i + 2] = brightness;
-                    // alpha
-                    // data[i + 3] = data[i + 3] > 120 ? data[i + 3] - 120 : 0;
-                }
-
-                ctx.putImageData(imageData, 0, 0);
-            }
-
-            resolve(ctx.canvas);
+            const canvas = image2canvas(img, owidth, oheight, isGrey);
+            resolve(canvas);
         }, reject);
     });
 };
+
+export function image2canvas(image, owidth, oheight, isGrey = false) {
+    const ctx = document.createElement('canvas').getContext('2d');
+    ctx.canvas.width = owidth;
+    ctx.canvas.height = oheight;
+    const { width, height, offsetX, offsetY } = cover(ctx.canvas.width, ctx.canvas.height, image.width, image.height);
+    ctx.drawImage(image, offsetX, offsetY, width, height);
+
+    if (isGrey) {
+        const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+        const { data } = imageData;
+        for (var i = 0; i < data.length; i += 4) {
+            // var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+            var brightness = 0.2126 * data[i] + 0.7152 * data[i + 1] + 0.0722 * data[i + 2];
+            // red
+            data[i] = brightness;
+            // green
+            data[i + 1] = brightness;
+            // blue
+            data[i + 2] = brightness;
+            // alpha
+            // data[i + 3] = data[i + 3] > 120 ? data[i + 3] - 120 : 0;
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    return ctx.canvas;
+}
