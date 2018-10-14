0c229578ee4fd9c08a7d301a2d8580c79a04dea6
Arka Roy <i.arkaroy@gmail.com>
2018-10-14 20:44:50 +0530

    Updated header style

--- src/js/components/shared/Header.js
+++ src/js/components/shared/Header.js
@@ -60,7 +60,6 @@ class Header extends Component {
                 <header className={styles['site-header']}>
 
                     <Link to="/" className={styles['home-link']}>
-
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400.00001" height="12" width="12">
                             <path d="M 167.48331,5e-4 20.41,400 86.033198,400 197.5616,79.29619 l 4.68481,0 45.47287,130.1334 61.84552,0 L 232.52164,5e-4 Z m 15.71651,250.80764 -18.2678,52.31472 115.24345,0 L 313.76886,400 379.589,400 324.75579,250.8678 Z" fill="white" />
                         </svg>
