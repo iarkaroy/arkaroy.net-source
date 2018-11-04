1607afb5a1fcc4d9ff27606fba114800566840fa
Arka Roy <i.arkaroy@gmail.com>
2018-11-05 00:14:20 +0530

    Cleaned up index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { render, hydrate } from 'react-dom';
 import "regenerator-runtime/runtime";
 
-// import './scss/index.scss';
-
 import App from './js/app';
 
 const root = document.getElementById('app');
